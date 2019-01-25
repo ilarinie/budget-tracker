28517fe30e336767bf747bb13d6a6a75af6b26b7
ilarinie <ilari.nieminen@outlook.com>
2019-01-25 11:58:45 +0200

    Tests

---
+++ test/src/graphql/context.spec.ts
@@ -0,0 +1,42 @@
+import chai, { assert, expect } from 'chai';
+import chaiPromise from 'chai-as-promised';
+import jwt from 'jsonwebtoken';
+import { ConnectionManager, createConnection, getConnection, getConnectionManager } from 'typeorm';
+import { closeConnectionIfNecessary, createConnectionIfNecessary } from '../..';
+import { User } from '../../../src/entity/User';
+import { context } from '../../../src/graphql';
+
+chai.use(chaiPromise);
+
+describe('Apollo server context spec', () => {
+  before(async () => {
+    await createConnectionIfNecessary();
+  });
+
+  after(async () => {
+    await closeConnectionIfNecessary();
+  });
+
+  it('can parse use from auth token', async () => {
+    const user = new User();
+    user.username = 'muupmaap';
+    user.password = 'password123';
+    await user.save();
+    const token = 'Bearer ' + jwt.sign(JSON.parse(JSON.stringify(user)), process.env.JWT_SECRET as string);
+    const ress = await context({ req: { headers: { authorization: token }}});
+    assert.equal(ress.user.username, user.username);
+  });
+
+  it('wont parse invalid auth token', async () => {
+    const user = new User();
+    user.username = 'muupmaap';
+    user.password = 'password123';
+    await user.save();
+    const token = 'Bearer ' + jwt.sign(JSON.parse(JSON.stringify(user)), 'asdasd' as string);
+    const headers = {
+      authorization: token
+    };
+
+    assert.isRejected(context({ req: { headers }}));
+  });
+});
