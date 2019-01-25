26ee491632230c99fc0c9f27cf21ad953b0cd6ab
ilarinie <ilari.nieminen@outlook.com>
2019-01-25 17:09:39 +0200

    Purchases sorting on user query.

--- src/graphql/queries/userQuery.ts
+++ src/graphql/queries/userQuery.ts
@@ -2,6 +2,12 @@ import { User } from '../../entity/User';
 
 export const user = async (parent, args, context) => {
   const id = context.user.id;
-  const user = await User.findOne({ id }, { relations: [ 'purchases', 'categories', 'purchases.categories' ]});
+  const user = await User.findOne({ id }, { relations: [ 'purchases', 'categories', 'purchases.categories' ]}) as User;
+  user.purchases = user.purchases.sort((a, b) => {
+    if (a.created_at == b.created_at) {
+      return 0;
+    }
+    return new Date(a.created_at).getMilliseconds() > new Date(b.created_at).getMilliseconds() ? 1 : -1;
+  });
   return user;
 };
