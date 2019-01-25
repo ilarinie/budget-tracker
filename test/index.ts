0f595d1d3958982b59d50ea09154327a3bd768a6
ilarinie <ilari.nieminen@outlook.com>
2019-01-25 12:06:41 +0200

    Safeguard tests.

--- test/index.ts
+++ test/index.ts
@@ -1,14 +1,21 @@
 import { createConnection, getConnection, getConnectionManager } from 'typeorm';
 
 export const createConnectionIfNecessary = async () => {
-  getConnection().close().catch(e => {
-    //NOP
-  });
+  try {
+    getConnection().close().catch(e => {
+      //NOP
+    });
+  } catch (e) {
+  }
   await createConnection();
   await getConnection().synchronize(true);
 };
 
 export const closeConnectionIfNecessary = async () => {
+  try {
     await getConnection().synchronize(true).catch(e => {});
-    getConnection().close().catch(e => {});
+  } catch (e) {
+
+  }
+  getConnection().close().catch(e => {});
 };
